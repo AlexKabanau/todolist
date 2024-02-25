fe420bf253b45ae0efc22ffa4f01b699f0ae0098
Alexandr Kabanau <Azraill1989@gmail.com>
2024-02-25 20:41:28 +0300

    feat: 1 create TodoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { TaskType, TodoList } from './TodoList';
 
 function App() {
+  let tasks1: Array<TaskType> = [
+    { id: 1, title: 'CSS+HTML', isDone: true },
+    { id: 2, title: 'JS', isDone: true },
+    { id: 3, title: 'React', isDone: false },
+  ];
+
+  let tasks2: Array<TaskType> = [
+    { id: 1, title: 'Terminator', isDone: true },
+    { id: 2, title: 'XXX', isDone: false },
+    { id: 3, title: 'Gentelments of Fortune', isDone: true },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TodoList</h1>
+      {/* <input type="checkbox" />
+      <input type="date" />
+      <input placeholder="todoList" /> */}
+      <TodoList title="What to learn" tasks={tasks1} />
+      <TodoList title="Movies" tasks={tasks2} />
     </div>
   );
 }
