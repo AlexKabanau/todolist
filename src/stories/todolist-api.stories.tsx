f3ee5380eb0caa5a8b84b28d161c432230f823c1
Alexandr Kabanau <Azraill1989@gmail.com>
2024-05-27 21:17:15 +0300

    add: add create/delete/update requests

--- src/stories/todolist-api.stories.tsx
+++ src/stories/todolist-api.stories.tsx
@@ -5,11 +5,10 @@ export default {
   title: 'API',
 };
 
-const instance = axios.create({
+const settings = axios.create({
   withCredentials: true,
-  baseURL: 'https://social-network.samuraijs.com/api/1.1/',
   headers: {
-    'API-KEY': 'a6235f60-17e5-4077-a023-12c9679f93bf',
+    'API-KEY': 'f4bcbf25-138b-4ec8-9796-314594a7e9cc',
   },
 });
 
@@ -17,19 +16,21 @@ export const GetTodoLists = () => {
   const [state, setState] = useState<any>([]);
   debugger;
 
-  axios
-    .get(`https://social-network.samuraijs.com/api/1.1/todo-lists`, {
-      withCredentials: true,
-      headers: {
-        'API-KEY': 'a6235f60-17e5-4077-a023-12c9679f93bf',
-      },
-    })
-    .then((res) => {
-      // debugger;
-      console.log('GET', res.data);
+  useEffect(() => {
+    axios
+      .get(`https://social-network.samuraijs.com/api/1.1/todo-lists`, {
+        withCredentials: true,
+        headers: {
+          'API-KEY': 'f4bcbf25-138b-4ec8-9796-314594a7e9cc',
+        },
+      })
+      .then((res) => {
+        debugger;
+        console.log('GET', res.data);
 
-      setState(res.data);
-    });
+        setState(res.data);
+      });
+  }, []);
 
   return <div>GET, {JSON.stringify(state)}</div>;
 };
@@ -44,7 +45,7 @@ export const CreateTodoList = () => {
       {
         withCredentials: true,
         headers: {
-          'API-KEY': 'a6235f60-17e5-4077-a023-12c9679f93bf',
+          'API-KEY': 'f4bcbf25-138b-4ec8-9796-314594a7e9cc',
         },
       },
     )
@@ -52,8 +53,55 @@ export const CreateTodoList = () => {
       // debugger;
       setState(res.data);
     });
+
+  return <div>SET (create), {JSON.stringify(state)}</div>;
 };
 
-export const DeleteTodoList = () => {};
+export const DeleteTodoList = () => {
+  const [state, setState] = useState<any>([]);
+  debugger;
 
-export const UpdateTodoList = () => {};
+  useEffect(() => {
+    axios
+      .delete(
+        `https://social-network.samuraijs.com/api/1.1/todo-lists/8b45af5d-616b-4282-9d1e-36349397404f`,
+
+        {
+          withCredentials: true,
+          headers: {
+            'API-KEY': 'f4bcbf25-138b-4ec8-9796-314594a7e9cc',
+          },
+        },
+      )
+      .then((res) => {
+        debugger;
+        console.log('DELETE', res.data);
+
+        setState(res.data);
+      });
+  }, []);
+
+  return <div>DELETE, {JSON.stringify(state)}</div>;
+};
+
+export const UpdateTodoList = () => {
+  const [state, setState] = useState<any>([]);
+
+  axios
+    .put(
+      `https://social-network.samuraijs.com/api/1.1/todo-lists/a0859e3c-4e54-4c0d-8ecf-833444eb196f`,
+      { title: 'YoYo' },
+      {
+        withCredentials: true,
+        headers: {
+          'API-KEY': 'f4bcbf25-138b-4ec8-9796-314594a7e9cc',
+        },
+      },
+    )
+    .then((res) => {
+      // debugger;
+      setState(res.data);
+    });
+
+  return <div>SET newTitle, {JSON.stringify(state)}</div>;
+};
