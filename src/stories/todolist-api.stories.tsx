afb3c6d9550398a8e5db267401c6124a1eee6b3f
AlexKabanau <azraill1989@gmail.com>
2024-05-27 15:33:28 +0300

    add: add get response

--- src/stories/todolist-api.stories.tsx
+++ src/stories/todolist-api.stories.tsx
@@ -14,18 +14,45 @@ const instance = axios.create({
 });
 
 export const GetTodoLists = () => {
-  const [state, setState] = useState<any>(null);
+  const [state, setState] = useState<any>([]);
   debugger;
 
-  instance.get(`todo-lists`).then((res) => {
-    debugger;
-    console.log(res);
-  });
+  axios
+    .get(`https://social-network.samuraijs.com/api/1.1/todo-lists`, {
+      withCredentials: true,
+      headers: {
+        'API-KEY': 'a6235f60-17e5-4077-a023-12c9679f93bf',
+      },
+    })
+    .then((res) => {
+      // debugger;
+      console.log('GET', res.data);
 
-  return <div>{JSON.stringify(state)}</div>;
+      setState(res.data);
+    });
+
+  return <div>GET, {JSON.stringify(state)}</div>;
 };
 
-export const CreateTodoList = () => {};
+export const CreateTodoList = () => {
+  const [state, setState] = useState<any>([]);
+
+  axios
+    .post(
+      `https://social-network.samuraijs.com/api/1.1/todo-lists`,
+      { title: 'string todoList' },
+      {
+        withCredentials: true,
+        headers: {
+          'API-KEY': 'a6235f60-17e5-4077-a023-12c9679f93bf',
+        },
+      },
+    )
+    .then((res) => {
+      // debugger;
+      setState(res.data);
+    });
+};
 
 export const DeleteTodoList = () => {};
 
